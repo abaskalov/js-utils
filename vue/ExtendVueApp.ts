cc1a90fd2ac92e936bc9ba811c66d140866c00c0
aaa <abaskalov@me.com>
2021-03-29 16:09:20 +0600

    fixes

--- vue/ExtendVueApp.ts
+++
@@ -1,8 +0,0 @@
-import { NuxtAppOptions } from "@nuxt/types/app";
-
-export default function ExtendVueApp(app: NuxtAppOptions, mixin: any): void {
-  if (!app.mixins) {
-    app.mixins = [];
-  }
-  app.mixins.push(mixin);
-}
