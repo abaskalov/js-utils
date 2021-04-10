af392577aa769be3b19ca032a97731a57bb31bad
aaa <abaskalov@me.com>
2021-04-10 19:33:36 +0600

    fixes

--- number/trim-decimal.js
+++ number/trim-decimal.js
@@ -4,7 +4,13 @@ exports.trimDecimal = void 0;
 var trimDecimal = function (v, d) {
     if (v === void 0) { v = 0; }
     if (d === void 0) { d = 2; }
-    var a = ((v * Math.pow(10, d)) / Math.pow(10, d)).toFixed(d);
-    return a * 1;
+    var re = new RegExp("^-?\\d+(?:.\\d{0," + (d || -1) + "})?");
+    try {
+        var matched = v.toString().match(re);
+        return matched !== null ? parseFloat(matched[0]) : 0;
+    }
+    catch (e) {
+        return 0;
+    }
 };
 exports.trimDecimal = trimDecimal;
--- number/trim-decimal.ts
+++ number/trim-decimal.ts
@@ -1,4 +1,9 @@
 export const trimDecimal = (v = 0, d = 2) => {
-  const a: any = ((v * Math.pow(10, d)) / Math.pow(10, d)).toFixed(d);
-  return a * 1;
+  const re = new RegExp("^-?\\d+(?:.\\d{0," + (d || -1) + "})?");
+  try {
+    const matched: string[] | null = v.toString().match(re);
+    return matched !== null ? parseFloat(matched[0]) : 0;
+  } catch (e) {
+    return 0;
+  }
 };
