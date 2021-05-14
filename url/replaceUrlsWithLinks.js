888649b285c2d07ebfe6220b87a5c2ca5e913f25
aaa <abaskalov@me.com>
2021-05-14 13:50:00 +0600

    links

--- url/replaceUrlsWithLinks.js
+++ url/replaceUrlsWithLinks.js
@@ -3,8 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.replaceUrlsWithLinks = void 0;
 var replaceUrlsWithLinks = function (content) {
     if (content === void 0) { content = ""; }
-    content = content.replace(/<a\b[^>]*>/gi, "").replace(/<\/a>/gi, "");
-    var expMatch = /(\b(https?|):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
-    return content.replace(expMatch, "<a target='_blank' href='$1'>$1</a>");
+    return content.replace(/([^\S]|^)(((https?:\/\/)|(www\.))(\S+))/gi, function (match, space, url) {
+        var hyperlink = url;
+        if (!hyperlink.match("^https?://")) {
+            hyperlink = "http://" + hyperlink;
+        }
+        return (space + '<a target="_blank" href="' + hyperlink + '">' + url + "</a>");
+    });
 };
 exports.replaceUrlsWithLinks = replaceUrlsWithLinks;
--- url/replaceUrlsWithLinks.ts
+++ url/replaceUrlsWithLinks.ts
@@ -1,6 +1,13 @@
-export const replaceUrlsWithLinks = (content: string = ""): string => {
-  content = content.replace(/<a\b[^>]*>/gi, "").replace(/<\/a>/gi, "");
-
-  const expMatch = /(\b(https?|):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|])/gi;
-  return content.replace(expMatch, "<a target='_blank' href='$1'>$1</a>");
-};
+export const replaceUrlsWithLinks = (content: string = ""): string =>
+  content.replace(
+    /([^\S]|^)(((https?:\/\/)|(www\.))(\S+))/gi,
+    function (match, space, url) {
+      let hyperlink = url;
+      if (!hyperlink.match("^https?://")) {
+        hyperlink = "http://" + hyperlink;
+      }
+      return (
+        space + '<a target="_blank" href="' + hyperlink + '">' + url + "</a>"
+      );
+    }
+  );
